253ae9bf1f91b72b736d57b888f2ccfe05096666
matthias <matthiasschmitt@Mac.local>
2015-06-05 23:43:53 +0200

    Datenbank test

--- app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/WelcomePageActivity.java
+++ app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/WelcomePageActivity.java
@@ -1,7 +1,9 @@
 package com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatCallback;
 import android.support.v7.app.AppCompatDelegate;
@@ -11,6 +13,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.Utilities.DebugUtils;
+import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.contentprovider.WeltkulturerbeContentProvider;
+import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.database.LongRouteTable;
 
 /**
  * This activity is the launch activity of the World-Heritage-Application
@@ -30,6 +36,7 @@ public class WelcomePageActivity extends Activity implements AppCompatCallback {
     public void onSupportActionModeFinished(ActionMode mode) {
         // let's leave this empty, for now
     }
+
     private AppCompatDelegate delegate;
 
     @Override
@@ -50,25 +57,25 @@ public class WelcomePageActivity extends Activity implements AppCompatCallback {
         delegate.setSupportActionBar(toolbar);
 
         Button btn_welcome_start = (Button) findViewById(R.id.btn_welcome_start);
-             btn_welcome_start.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
+        btn_welcome_start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 startActivity(new Intent(WelcomePageActivity.this, InstructionActivity.class));
-                                            }
-                                                                            });
+            }
+        });
 
         Button btn_welcome_continue = (Button) findViewById(R.id.btn_welcome_continue);
-            btn_welcome_continue.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                startActivity(new Intent(WelcomePageActivity.this, QuizActivity.class));
-                                            }
-                                                                             });
+        btn_welcome_continue.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                query();
+            }
+        });
 
         Button btn_welcome_score = (Button) findViewById(R.id.btn_welcome_score);
-            btn_welcome_score.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                startActivity(new Intent(WelcomePageActivity.this, ScoreActivity.class));
-                                            }
-                                                                            });
+        btn_welcome_score.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                insert();
+            }
+        });
     }
 
     @Override
@@ -84,5 +91,32 @@ public class WelcomePageActivity extends Activity implements AppCompatCallback {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void insert()
+    {
+        ContentValues values =new ContentValues();
+        values.put(LongRouteTable.COLUMN_NAME,"CG");
+        values.put(LongRouteTable.COLUMN_LONGITUDE,23.34);
+        values.put(LongRouteTable.COLUMN_LATITUDE,50.70);
+        getContentResolver().insert(WeltkulturerbeContentProvider.URI_TABLE_LONG_ROUTE,values);
+    }
+
+    public void query()
+    {
+        String projection[]={LongRouteTable.COLUMN_NAME,LongRouteTable.COLUMN_LONGITUDE,LongRouteTable.COLUMN_LATITUDE};
+        String selection=LongRouteTable.COLUMN_NAME+"=?";
+        String selectionArgs[]={"Bamberger Dom"};
+        String sortOrder=null;
+        Cursor cursor=getContentResolver().query(WeltkulturerbeContentProvider.URI_TABLE_LONG_ROUTE,projection,selection,selectionArgs,sortOrder);
+        String result="";
+        while(cursor.moveToNext())
+        {result+=cursor.getString(cursor.getColumnIndex(LongRouteTable.COLUMN_NAME));
+            result+=", "+cursor.getFloat(cursor.getColumnIndex(LongRouteTable.COLUMN_LONGITUDE));
+            result+=", "+cursor.getFloat(cursor.getColumnIndex(LongRouteTable.COLUMN_LATITUDE));
+            result+="/n";
+                DebugUtils.toast(this,result);
+
+        }
     }
 }
